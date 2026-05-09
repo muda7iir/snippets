cbe02117cb295d6c16359e830e69d72e53bbf2f9
muda7iir <muda7iir@users.noreply.github.com>
2026-05-09 17:26:22 +0500

    chore: update nx modules version to 19.0.1.0.0

--- nx_base_theme/__manifest__.py
+++ nx_base_theme/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'NX Base Theme',
-    'version': '1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Theme/Website',
     'summary': 'Base theme components',
     'depends': ['website'],
--- nx_ui_components/__manifest__.py
+++ nx_ui_components/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'NX UI Components',
-    'version': '1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Website',
     'summary': 'Reusable UI Components',
     'depends': ['website'],
